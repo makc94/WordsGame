6ee3adfcdfabc1810510eefc842a524f5d50dee3
VladyslavNikolenko <75621593+VladyslavNikolenko@users.noreply.github.com>
2022-06-28 17:18:00 +0300

    Our first project

--- app/src/main/java/com/dvl/words2guess/MainActivity2.java
+++ app/src/main/java/com/dvl/words2guess/MainActivity2.java
@@ -2,6 +2,7 @@ package com.dvl.words2guess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.Objects;
@@ -13,5 +14,8 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        Intent intent = new Intent (this, MainActivity3.class);
+        startActivity(intent);
     }
 }
